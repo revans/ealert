443acc282c5b1542cdc5fd3852c6517841769c21
Robert R Evans <robert@codewranglers.org>
2010-07-08 18:59:07 -0700

    Updates

--- lib/ealert/twitter_filter.rb
+++ lib/ealert/twitter_filter.rb
@@ -5,10 +5,14 @@ module EAlert
     ##
     # By Keywords
     #
-    def self.by_keywords(keywords, auth=[])
-      file      = File.dirname(__FILE__) + '/tweets' 
+    def self.by_keywords(keywords, location, auth=[])
+      file      = File.expand_path(location) + '/tweets' 
       filters   = keywords.split(',').collect! { |w| w.strip }.join(',') # TODO: Sanitize
       filename  = keywords.split(',').collect! { |w| w.strip }.join('-')
+
+      `mkdir -p #{file}` unless File.exists?(file)
+      
+      STDOUT.puts "Going to start streaming tweets that talk about #{filters} and store them in this file: #{file}."
   
       EventMachine.run do
         http = ::EventMachine::HttpRequest.new("http://stream.twitter.com/1/statuses/filter.json").get(
@@ -16,7 +20,6 @@ module EAlert
           :query  => {:track          => filters}
         )
         http.stream do |chunk| 
-          # ::EventStreamer::Parser.json(chunk)
           File.open(File.join(file,"#{Date.today.to_s}_#{filename}.json"), 'a+') { |f| f.write(chunk) }
         end
       end
