cc11f3d27ba44f3183b3c11699726cdfc2e98b9a
Robert R Evans <robert@codewranglers.org>
2010-07-09 16:48:23 -0700

    adjustments to the config

--- lib/ealert.rb
+++ lib/ealert.rb
@@ -1,6 +1,6 @@
 $:.unshift(File.dirname(__FILE__)) unless $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
-libraries = %w(date ostruct optparse)
+libraries = %w(date ostruct optparse yaml)
 libraries.each { |lib| require lib }
 
 # require 'rubygems'
--- lib/ealert/stream.rb
+++ lib/ealert/stream.rb
@@ -1,5 +1,20 @@
 module EAlert
   class Stream
     
+    def self.event(name)
+      @config = ::YAML.load(File.join(::EAlert::EVENT_DIR, 'events.yaml'))[name.to_sym]
+      @config
+    end
+    
   end
 end
+
+
+# keywords = ARGV.first
+#   location = ARGV.last
+#   pid = fork do
+#     Signal.trap('HUP', 'IGNORE')
+#     ::EAlert::TwitterFilter.by_keywords(keywords, location, ['revans', ''])
+#   end
+#   Process.detach(pid)
+#   puts "Setup as a background process with PID: #{pid}."
