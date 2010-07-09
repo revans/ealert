eea75c1c542930066077ceacd090547ee5b2d46c
Robert R Evans <robert@codewranglers.org>
2010-07-09 16:11:58 -0700

    Fixed the file generation

--- lib/ealert/config.rb
+++ lib/ealert/config.rb
@@ -6,7 +6,7 @@ module EAlert
       # Generate the yaml file that will be used to stream tweets
       #
       def generate!
-        file      = File.join(File.expand_path(File.dirname(__FILE__)), '../config/stream.yml')
+        file      = File.join(File.expand_path(File.dirname(__FILE__)), '../config/stream.yaml')
         write_to  = File.join(File.expand_path("~"), '.stream.yaml')
         
         File.open(write_to, 'w+') { |f| f.write(File.read(file).chomp!) }
