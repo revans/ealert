0797a8a9844945ab483bfe811313567003220f6b
Robert R Evans <robert@codewranglers.org>
2010-07-09 16:36:44 -0700

    Updated gemspec

--- lib/ealert/config.rb
+++ lib/ealert/config.rb
@@ -7,7 +7,7 @@ module EAlert
       #
       def generate!
         file      = File.join(File.expand_path(File.dirname(__FILE__)), '../config/events.yaml')
-        `mkdir -p #{dir}` unless File.exists?(::EAlert::EVENT_DIR)
+        `mkdir -p #{::EAlert::EVENT_DIR}` unless File.exists?(::EAlert::EVENT_DIR)
         write_to  = File.join(::EAlert::EVENT_DIR, 'events.yaml')
         File.open(write_to, 'w+') { |f| f.write(File.read(file)) }
       end
