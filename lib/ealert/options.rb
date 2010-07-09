0f2f6a95f3424dad1d0558a944117e68fffccac2
Robert R Evans <robert@codewranglers.org>
2010-07-09 15:37:09 -0700

    Adding option parsing

--- lib/ealert/options.rb
+++ lib/ealert/options.rb
@@ -3,15 +3,14 @@ module EAlert
     
     def self.parse(args)
       options = OpenStruct.new
-      options.library       = []
-      options.inplace       = false
-      options.encoding      = "utf8"
-      options.transfer_type = :auto
-      options.verbose       = false
       
       opts = ::OptionParser.new do |opts|
         opts.on('-g', '--generate-config', "Generate the config file.") do
           ::EAlert::Config.generate!
+        end
+        
+        opts.on('-r', '--run event', 'Run all events in your YAML file, or pick a specific one.') do |e|
+          options.event = e
         end
         
         opts.on_tail("-h", "--help", "Show this message") do
---
+++ lib/ealert/stream.rb
@@ -0,0 +1,5 @@
+module EAlert
+  class Stream
+    
+  end
+end
