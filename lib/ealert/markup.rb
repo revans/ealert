974166aded2e91c6a322181400c9234ed76dcbd4
Robert R Evans <robert@codewranglers.org>
2010-07-22 09:25:08 -0700

    Added text parsing so links become links and twitter names become twitter links to their accounts

--- lib/ealert/markup.rb
+++ lib/ealert/markup.rb
@@ -8,12 +8,26 @@ module EAlert
     <img src="#{tweet[:user][:profile_image_url]}" alt="#{tweet[:user][:screen_name]} Avatar" />
   </div>
   <div class='tweet_text'>
-    <p>#{tweet[:text]}</p>
+    <p>#{linkify(tweet[:text])}</p>
     <p>#{tweet[:user][:screen_name]} tweeted this on #{tweet[:created_at]}</p>
   </div>
 </div>
       HTML
     end
+    
+    private
+      
+      
+      def self.linkify(text)
+        text.split(' ').each do |word|          
+          if word =~ /^http|https|www\./
+            word.gsub!(word.to_s, "<a href='#{word}'>#{word}</a>")
+          elsif word =~ /^@\w/
+            name = word.to_s.gsub(/^@/, '')
+            word.gsub!(word.to_s, "<a href='http://www.twitter.com/#{name}>#{word}</a>")
+          end
+        end.join(' ')
+      end
     
   end
 end
